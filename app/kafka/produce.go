febb1304e3e3a094c6d97e62a0cdace801a9ff62
salesof7 <jpsaless2002@gmail.com>
2023-03-23 13:31:41 -0300

    feat: create function that execute the production

---
+++ app/kafka/produce.go
@@ -0,0 +1,27 @@
+package kafka
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	route "github.com/salesof7/go_kafka-simulator-fs/app/routes"
+	"github.com/salesof7/go_kafka-simulator-fs/infra/kafka"
+)
+
+func Produce(msg *ckafka.Message) {
+	producer := kafka.NewKafkaProducer()
+	route := route.NewRoute()
+	json.Unmarshal(msg.Value, &route)
+	route.LoadPositions()
+	positions, err := route.ExportJsonPositions()
+	if err != nil {
+		log.Println(err.Error())
+	}
+	for _, p := range positions {
+		kafka.Publish(p, os.Getenv("KafkaProduceTopic"), producer)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
